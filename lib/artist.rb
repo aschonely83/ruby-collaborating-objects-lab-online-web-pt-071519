3e303c0c7c6a9f181ff48f11e30d3d6dab2f1fbf
Adam Schonely <aschonely@gmail.com>
2019-08-17 03:28:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,8 +20,8 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
   
-  def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
+  def self.find_or_create_by_name(name)
+    self
   end
   
   def print_songs
