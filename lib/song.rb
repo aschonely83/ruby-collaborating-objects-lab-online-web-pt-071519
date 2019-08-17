ca2768f5974b8bac110f43ea836d70f1c51305e9
Adam Schonely <aschonely@gmail.com>
2019-08-17 03:35:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,13 +12,13 @@ class Song
    @@all
   end
  
-  def self.new_by_filename(file)
-    song, artist = filename.split(" - ")
-    new_song = self.new(song_name)
+    def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
     new_song.artist_name = artist
     new_song
   end
- 
+
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
